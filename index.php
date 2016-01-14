cc647446adbce5054eaa34b340ce2b75c27091d0
Elaine Thomas <enajera2@cnm.edu>
2016-01-14 15:19:46 -0700

    index page with svg file containing ERD for pcpartpickerdotcom

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			ERD for PC Part Picker Website
+		</title>
+	</head>
+	<body>
+		<h1>Entity Relationship Model for PC Part Picker Website</h1>
+<img src="pcpartpickerdotcom.svg" />
+	</body>
+</html>
