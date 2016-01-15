d714c762275de6dac9a83622b3d231d88d7dbb88
Elaine Thomas <enajera2@cnm.edu>
2016-01-14 21:34:53 -0700

    index page with svg file containing ERD for pcpartpickerdotcom

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
+		<link href="styles1.css" type="text/css" rel="stylesheet" />
+		<link href='https://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
 		<title>
 			ERD for PC Part Picker Website
 		</title>
 	</head>
 	<body>
-		<h1>Entity Relationship Model for PC Part Picker Website</h1>
-<img src="pcpartpickerdotcom.svg" />
+		<p>
+			<a href="http://pcpartpicker.com/">PC Part Picker Website</a>
+		</p>
+		<h1>Entity Relationship Diagram for PC Part Picker Website</h1>
 	</body>
 </html>
