c872f9ee70f8c532513fb3c239e0fa165c030b4b
Elaine Thomas <enajera2@cnm.edu>
2016-01-16 10:16:27 -0700

    update wording, added back photo, changed bg

--- index.php
+++ index.php
@@ -2,25 +2,44 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<!--<link href="styles1.css" type="text/css" rel="stylesheet" />-->
+		<link href="styles1.css" type="text/css" rel="stylesheet" />
 		<link href='https://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
 		<title>
 			ERD for PC Part Picker Website
 		</title>
 	</head>
 	<body>
-		<h3>A DIY PC Builder</h3>
-		<img src="pcpartpickerdotcom.svg" />
-			<h4>A student on a budget</h4>
-		<div>
-			<p>
-				DIY PC Builder has been building PCs since she was 12 years old.  While she is confident in picking out
-			</p>
+		<header>
+			<h1>Entity Relationship Diagram for PC Part Picker Website</h1>
+		</header>
+		<img src="pcpartpickerdotcom.svg" class="erdSvg"/>
+		<img src="avatar-me.png" class="userPhoto"/>
+		<h2>DIY PC Builder</h2>
+			<h3>A student on a budget</h3>
+			<div>
+				<p>
+				DIY PC Builder has been building PCs since she was 12 years old.  She knows what types of 				computer components she wants to include in her next build - a computer for school work and gaming.
+				</p>
+			<h4>Needs</h4>
+				<ol>
+					<li>Meet minimum requirements for build
+						<ul>
+							<li>correct amount of RAM</li>
+							<li>compatible with system build</li>
+						</ul>
+					</li>
+					<li>Keep the RAM chip cost below 60$ to fit budget</li>
+					<li>Get the best possible product based on user ratings</li>
+				</ol>
+			<h4>Frustrations</h4>
+				<p>
+					DIY PC Builder dislikes doing the time consuming research necessary to purchase individual computer components for a complete computer build.  The RAM chip she will ultimately decide to purchase should be compatible with her motherboard and also be the best rated by other experienced PC Builders.
+				</p>
 		</div>
 	</body>
 	<footer>
 		<a href="http://pcpartpicker.com/">PC Part Picker Website</a><br><br>
 		&copy; 2016 el41net
-		<h1>Entity Relationship Diagram for PC Part Picker Website</h1>
+
 	</footer>
 </html>
