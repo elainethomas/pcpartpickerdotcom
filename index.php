45bd914050698c724096f7db72dfc53caf675dd7
Elaine Thomas <enajera2@cnm.edu>
2016-01-16 20:40:12 -0700

    changed formatting on images to align with text

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
 			<h1>Entity Relationship Diagram for PC Part Picker Website</h1>
 		</header>
 		<img src="pcpartpickerdotcom.svg" class="erdSvg"/>
-		<div id="sidebar">
+		<aside>
 		<img src="avatar-me.png" class="userPhoto"/>
 		<h2>DIY PC Builder</h2>
 			<h3>A student on a budget</h3>
 				<p>
-				DIY PC Builder has been building PCs since she was 12 years old.  She knows what types of 				computer components she wants to include in her next build - a computer for school work and gaming.
-				</p>
+				DIY PC Builder has been building PCs since she was 12 years old.  She already has most of the necessary components and she already knows what size of RAM she will need to support a scientifc graphing program she needs for school and her favorite PC games.
+				</p><br>
 			<h4>Needs</h4>
 				<ol>
 					<li>Meet minimum requirements for build
@@ -35,7 +35,7 @@
 				<p>
 					DIY PC Builder dislikes doing the time consuming research necessary to purchase individual computer components for a complete computer build.  The RAM chip she will ultimately decide to purchase should be compatible with her motherboard and also be the best rated by other experienced PC Builders.
 				</p>
-		</div>
+		</aside>
 	</body>
 		<footer>
 		<a href="http://pcpartpicker.com/">PC Part Picker Website</a><br><br>
